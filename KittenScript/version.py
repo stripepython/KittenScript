8f3f0a7b360ddea151e965ae6225debd51c9c80b
stripepython <98206176+stripepython@users.noreply.github.com>
2023-01-20 12:05:34 +0800

    Update version.py

--- KittenScript/version.py
+++ KittenScript/version.py
@@ -2,7 +2,7 @@ from collections import namedtuple
 
 VersionClass = namedtuple('version', ('major', 'minor', 'micro'))
 
-version = VersionClass(1, 0, 5)
+version = VersionClass(1, 1, 2)
 
 def get_version():
     return '.'.join(map(str, version))
